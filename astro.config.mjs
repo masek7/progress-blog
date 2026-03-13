42e89df84bfb4b0c2320982ced6a694f3b7ed46b
Guilherme Morais <gui.moraes29@gmail.com>
2026-03-13 00:07:24 -0300

    corrigindo bug do css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ export default defineConfig({
   // Substitua pelo seu usuário real do GitHub
   site: 'https://masek7.github.io',
   // Substitua pelo nome exato do repositório que você criou no GitHub
-  base: 'progress-blog',
+  base: '/progress-blog',
 });
